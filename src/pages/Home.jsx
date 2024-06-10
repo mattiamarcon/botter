bee1cdbb270e883a0a9493ef132439da36743bcc
mattiamarcon <mattiamarcon05@gmail.com>
2024-06-10 22:30:09 +0200

    continuo e sistemazione homePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,11 +11,11 @@ export default function Home(){
             <Navbar />
             <div className="md:relative">
                 <div className="md:absolute z-10 md:mt-24 md:ml-20 md:w-2/3 lg:w-1/2 xl:w-1/3">
-                    <motion.h1 initial={{opacity:0, y:-100}} whileInView={{opacity:1,y:0}} transition={{duration:0.6}} className="text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl md:text-left text-center font-bold m-5 md:m-0">
-                       <mark className="inset-0 bg-white">CONCESSIONARIA AUTOMOBILISTICA E OFFICINA SPECIALIZZATA</mark>
+                    <motion.h1 initial={{opacity:0, y:-100}} whileInView={{opacity:1,y:0}} transition={{duration:0.6}} className="text-4xl sm:text-5xl md:text-6xl xl:text-7xl md:text-left text-center font-bold m-5 md:m-0">
+                    <mark className="bg-transparent md:bg-white leading-tight">CONCESSIONARIA AUTOMOBILISTICA E OFFICINA SPECIALIZZATA</mark>
                     </motion.h1>  
                 </div>
-                <div className="h-80 md:h-[600px] opacity-75">
+                <div className="h-56 sm:h-72 md:h-[600px]">
                     <Carousel leftControl={<MdArrowBackIosNew color="white" />} rightControl={<MdArrowForwardIos color="white" />} pauseOnHover>
                         <img src="foto/negozio.jpg" alt="..." />
                         <img src="foto/NegozioTreMacchine.jpg" alt="..." />
@@ -24,6 +24,29 @@ export default function Home(){
                     </Carousel>
                 </div>
             </div>
+            <div className="flex flex-col md:flex-row w-full lg:justify-evenly items-center my-5">
+                <motion.p initial={{opacity:0,x:-50}} whileInView={{opacity:1,x:0}} transition={{duration:1}} className="text-center w-3/4 mx-auto text-2xl sm:text-3xl md:text-4xl xl:text-5xl font-semibold my-3 md:w-1/3 xl:w-1/6">
+                    Il nostro pensiero è riassumibile in 3 parole: Professionalità, Serietà e Cortesia.
+                </motion.p>
+                <img src="foto/verticalLancia.jpg" alt="" className="h-96 md:h-fit md:w-1/3 my-3 mx-auto" />
+            </div>
+            <div className="w-full flex flex-col">
+                <p className="text-center w-3/4 mx-auto text-3xl md:text-4xl xl:text-5xl font-semibold my-3">
+                    Le nostre auto
+                </p>
+                <div className="w-2/3 sm:w-1/3 md:w-full lg:w-5/6 2xl:w-full flex flex-col md:flex-row justify-evenly flex-wrap mx-auto">
+                    <motion.img src="logos/abarth.png" initial={{opacity:0}} transition={{duration:2}} whileInView={{opacity:1}} className="md:h-52 h-44 my-4 " />      
+                    <motion.img src="logos/fiat.png" initial={{opacity:0}} transition={{duration:2}} whileInView={{opacity:1}} className="md:h-52 h-44 my-4" />
+                    <motion.img src="logos/jeep.png" initial={{opacity:0}} transition={{duration:2}} whileInView={{opacity:1}} className="md:h-52 h-44 my-4 " />
+                    <motion.img src="logos/lancia.png" initial={{opacity:0}} transition={{duration:2}} whileInView={{opacity:1}} className="md:h-52 md:w-fit h-32 w-2/3 my-4  " />
+                    <motion.img src="logos/suzuki.png" initial={{opacity:0}} transition={{duration:2}} whileInView={{opacity:1}} className="md:h-52 md:w-fit h-32 w-2/3 my-4  " />
+                    <motion.img src="logos/alfaromeo.png" initial={{opacity:0}} transition={{duration:2}} whileInView={{opacity:1}} className="md:h-52 h-44 my-4 bg-gray-600" />
+                </div>
+            </div>
+            
+            
+
+            
             <Footer />
             
         </>
@@ -31,7 +54,23 @@ export default function Home(){
 }
 
 /*
-<motion.h1 initial={{opacity:0, y:-100}} whileInView={{opacity:1,y:0}} transition={{duration:0.6}} className="text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl md:text-left text-center font-bold m-5 md:m-0">
-                       <mark className=" bg-white">CONCESSIONARIA AUTOMOBILISTICA E OFFICINA SPECIALIZZATA</mark>
-                    </motion.h1>  
+<div className="w-full flex-nowrap">
+                    <ul className="flex flex-row">
+                        <li>
+                            <img src="logos/abarth.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="logos/alfaromeo.jpg" alt="" />
+                        </li>
+                        <li>
+                            <img src="logos/fiat.png" alt="" />
+                        </li>
+                        <li>
+                            <img src="logos/jeep.svg" alt="" />
+                        </li>
+                        <li>
+                            <img src="logos/lancia.svg" alt="" />
+                        </li>
+                    </ul>
+                </div>
 */
